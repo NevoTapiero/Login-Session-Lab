2df2a067b0a464bebe839b8b51dcf4dcf13f29b4
NevoTapiero <nevotap1234@gmail.com>
2022-07-28 16:49:54 +0300

    quote

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,10 +4,23 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+#login_session["quote": "theQuote", "quote's author": "theAuthor", "author's name": "theName"]
+
+@app.route('/', methods = ['GET', 'POST']) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
+	if request.method == 'POST':
+		try:
+			login_session['quote'] = request.form['quote']
+			login_session["quotes_author"] = request.form["quotes_author"]
+			login_session["authors_age"] = request.form["authors_age"]
+			if login_session['quote'] == '' or login_session["quotes_author"] == '' or login_session["authors_age"] == '':
+				return redirect(url_for('error'))
+			else:
+				return redirect(url_for('thanks'))
+		except:
+			return redirect(url_for('error'))
+	else:
+		return render_template('home.html')
 
 
 @app.route('/error')
@@ -19,13 +32,12 @@ def error():
 @app.route('/display')
 def display():
 
-	return render_template('display.html', ) # What variables are needed?
+	return render_template('display.html', login_session = login_session) # What variables are needed?
 
 
 @app.route('/thanks')
 def thanks():
-
-	return render_template('thanks.html')
+	return render_template('thanks.html', login_session = login_session)
 
 
 if __name__ == '__main__':
